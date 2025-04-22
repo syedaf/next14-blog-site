adf387f4e49f6f680137db496eaa4e2f37a22575
Syed <sygaki786@gmail.com>
2025-04-21 21:24:10 -0400

    CSS changes

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -5,7 +5,12 @@ const AboutPage = () => {
   return (
     <div>
       <div className={styles.imgContainer}>
-        <Image src="/about.png" alt="" width={500} height={500} />
+        <Image
+          src="https://images.pexels.com/photos/31707366/pexels-photo-31707366/free-photo-of-majestic-view-of-mont-saint-michel-abbey.jpeg"
+          alt=""
+          width={500}
+          height={500}
+        />
       </div>
     </div>
   );
